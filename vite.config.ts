30550adc2248efa7a50fd2bdb1aac906cb7a6692
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-03-12 20:01:25 +0800

    chore(unittest): support vitest(vscode vitest plugin)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import chokidar from 'chokidar';
 // @ts-ignore
@@ -32,6 +32,10 @@ const generatePagePath = _.debounce(() => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+  },
   plugins: [
     react(),
     {
